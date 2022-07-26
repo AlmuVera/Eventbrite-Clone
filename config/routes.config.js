e5774da7c245de733e0557d329ee282de6aba73d
Almudena <almudena.vigara@gmail.com>
2022-07-26 14:16:55 +0200

    feat:creted the routes for CRD, not update yet

--- config/routes.config.js
+++ config/routes.config.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const events = require('../controllers/events.controller.js')
 
-router.get('/', events.list);
-router.get("/new", events.new);
-// router.get('/new', events.new);
-
+router.get('/', events.list); //render event list with filters
+router.get('/:id', events.detail) //render event detail
+router.get("/new", events.new); //render event form
+router.post('/', events.create); //create event and redirect to list
+router.get('/:id/delete', events.delete) //render event detail
 
 
 module.exports = router;
+
+//update Carlos dijo que de momento no
